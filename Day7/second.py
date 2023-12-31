f28bdf996832cb87606c2730bb6bfb48e12a2a67
kkub <krzysztof.kubieniec@hotmail.com>
2023-12-31 14:47:42 +0100

    day 7 part two done

---
+++ Day7/second.py
@@ -0,0 +1,49 @@
+from input_reader import read_file
+from collections import Counter
+
+lines = read_file("input.txt")
+cards_hierarchy = "J23456789TQKA"  ##13 elements
+
+
+def get_card_value(char):
+    return cards_hierarchy.index(char) + 2
+
+
+def count_card_values(hand):
+    return sum([get_card_value(card) * pow(100, len(hand) - index - 1) for index, card in enumerate(hand)])
+
+
+class Action:
+    def __init__(self, hand, bid):
+        self.hand = hand
+        self.bid = bid
+        self.order = self.calculate_order(hand)
+
+    @staticmethod
+    def calculate_order(hand):
+        hand_without_jokers = hand.replace('J', "")
+        char_count = Counter(hand_without_jokers)
+        length_of_count = len(char_count)
+        if length_of_count == 0:
+            hand_without_jokers = "JJJJJ"
+            char_count = Counter(hand_without_jokers)
+            length_of_count = len(char_count)
+            biggest_count = max(char_count.values())
+            return (length_of_count * 10000000000) + (biggest_count * -10000000000) - count_card_values(hand)
+        biggest_count = max(char_count.values())
+        return (length_of_count * 10000000000) + (biggest_count * -10000000000) - count_card_values(hand) - (10000000000 * Counter(hand).get("J", 0))
+
+    def __repr__(self):
+        return f'{self.hand}, order: {self.order}'
+
+
+def process(lines):
+    actions = sorted(
+        [Action("".join(line.split(" ")[0]), int(line.split(" ")[1].strip())) for line in lines],
+        key=lambda action: action.order)
+    for action in actions:
+        print(action)
+    return sum([action.bid * (len(actions) - index) for index, action in enumerate(actions)])
+
+
+print(process(lines))

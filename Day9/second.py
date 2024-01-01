3b1c5a47aefa7419b59f4fd9b764104a86aa075e
kkub <krzysztof.kubieniec@hotmail.com>
2024-01-01 19:04:05 +0100

    day 9 first two done

---
+++ Day9/second.py
@@ -0,0 +1,22 @@
+from input_reader import read_file
+
+lines = read_file("test.txt")
+
+
+def calc_val(numbers):
+    first_numbers = [numbers[0]]
+    while True:
+        numbers = [numbers[i] - numbers[i + 1] for i in range(len(numbers) - 1)]
+        first_numbers.append(numbers[0])
+
+        if len(set(numbers)) == 1:
+            break
+
+    return sum(first_numbers)
+
+
+def process(lines):
+    return sum([calc_val([int(item) for item in line.strip().split(" ")]) for line in lines])
+
+
+print(process(lines))

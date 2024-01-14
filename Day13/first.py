60ae87b00626e9e957462dc46421e1c191490e0a
kkub <krzysztof.kubieniec@hotmail.com>
2024-01-14 17:58:46 +0100

    clean up

--- Day13/first.py
+++ Day13/first.py
@@ -2,22 +2,13 @@ from input_reader import read_file
 
 
 def find_reflection_index(strings):
-    middle_index = len(strings) / 2
-    nearest_reflection_to_middle = None
     for i in range(1, len(strings)):
         left = strings[0:i][::-1]
         right = strings[i:len(strings)]
         match = all(item1 == item2 for item1, item2 in zip(left, right))
         if match:
-            if nearest_reflection_to_middle is None:
-                nearest_reflection_to_middle = i
-            elif abs(middle_index - i) < abs(middle_index - nearest_reflection_to_middle):
-                nearest_reflection_to_middle = i
-
-    if nearest_reflection_to_middle is not None:
-        print(f'({nearest_reflection_to_middle}, {nearest_reflection_to_middle + 1})')
-
-    return nearest_reflection_to_middle
+            print(f'({i}, {i + 1})')
+            return i
 
 
 def process_pattern(lines):
